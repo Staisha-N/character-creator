3f1ae654f3b9b21dc60f5cf71997385cd728723f
STAISHA NEVILLE <sjnevill@uwaterloo.ca>
2025-10-25 21:12:09 -0400

    Moved working code to main.py

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from langchain_ollama import ChatOllama
 from pydantic import BaseModel, Field
-from typing_extensions import TypedDict
 from langchain.tools import tool
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph import MessagesState
+from langchain.messages import SystemMessage, HumanMessage, ToolMessage
 
-llm = ChatOllama(model="llama3.1")
+llm = ChatOllama(model="llama3.2")
+
+class State(TypedDict):
+    strength: int
+    dexterity: int
+    constitution: int
+    intelligence: int
+    wisdom: int
+    charisma: int
 
 class AbilityScores(BaseModel):
-    strength: int = Field(None, description="Strength score")
-    dexterity: int = Field(None, description="Dexterity score")
-    constitution: int = Field(None, description="Constitution score")
-    intelligence: int = Field(None, description="Intelligence score")
-    wisdom: int = Field(None, description="Wisdom score")
-    charisma: int = Field(None, description="Charisma score")
+    strength: str = Field("low", description="Strength score")
+    dexterity: str = Field("low", description="Dexterity score")
+    constitution: str = Field("low", description="Constitution score")
+    intelligence: str = Field("low", description="Intelligence score")
+    wisdom: str = Field("low", description="Wisdom score")
+    charisma: str = Field("low", description="Charisma score")
+
+# Augment the LLM with schema for structured output
+structured_llm = llm.with_structured_output(AbilityScores)
+
+# Invoke the augmented LLM
+output = structured_llm.invoke("Consider a Dungeons and Dragons character that excels at physical combat. Rate the importance its six abilities (strength, dexterity, constitution, wisdom, intelligence and charisma) as 'high', 'medium' or 'low'.")
+
+print(output)
 
 @tool
-def rate(stg: str, dex: str, con: str, inte: str, wis: str, cha: str) -> list[str]:
-    """Rate the importance of stg, dex, con, int, wis and cha.
+def quantitative_scores(stg: str, dex: str, con: str, inte: str, wis: str, cha: str) -> list[int]:
+    """Create quantitative scores for these abilities: stg, dex, con, int, wis and cha.
 
     Args:
         stg: strength
@@ -25,12 +44,53 @@ def rate(stg: str, dex: str, con: str, inte: str, wis: str, cha: str) -> list[st
         wis: wisdom
         cha: charisma
     """
-    decision = [stg, dex, con, inte, wis, cha]
-    print(decision)
-    return decision
+    return [1,2,3,4,5,6]
 
-tools = [rate]
-llm_with_tools = llm.bind_tools(tools)
+# Augment the LLM with tools
+tools = [quantitative_scores]
+llm_with_tools = llm.bind_tools([quantitative_scores])
+tools_by_name = {tool.name: tool for tool in tools}
 
-# Invoke the augmented LLM
-output = llm_with_tools.invoke("Consider a character that excels at combat. Rate the importance its six abilities (strength, dexterity, constitution, wisdom, intelligence and charisma) as high, medium or low.")
+
+
+def llm_call(state: MessagesState):
+    """LLM decides whether to call a tool or not"""
+
+    return {
+        "messages": [
+            llm_with_tools.invoke(
+                [
+                    SystemMessage(
+                        content="You are a helpful assistant. Your input is a qualitative description of a characters' abilities and you must call a tool to get corresponding quantitative ability scores."
+                    )
+                ]
+                + state["messages"]
+            )
+        ]
+    }
+
+
+def tool_node(state: dict):
+    """Performs the tool call"""
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call["args"])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+
+agent_builder = StateGraph(MessagesState)
+
+agent_builder.add_node("llm_call", llm_call)
+agent_builder.add_node("tool_node", tool_node)
+
+agent_builder.add_edge(START, "llm_call")
+agent_builder.add_edge("llm_call", "tool_node")
+agent_builder.add_edge("tool_node", END)
+
+agent = agent_builder.compile()
+
+messages = [HumanMessage(content="strength='low' dexterity='high' constitution='high' intelligence='low' wisdom='medium' charisma='medium'")]
+messages = agent.invoke({"messages": messages})
+for m in messages["messages"]:
+    m.pretty_print()
