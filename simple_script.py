45e62221d348ffbe5f9b6bac6c7b95ee419d09e3
STAISHA NEVILLE <sjnevill@uwaterloo.ca>
2025-11-09 20:42:39 -0500

    Deleted GitHub pages setup due to incompatibility issues

--- simple_script.py
+++
@@ -1,6 +0,0 @@
-def simple_script(user_question):
-    from langchain_ollama import OllamaLLM
-    llm = OllamaLLM(model="llama3.2")
-    
-    answer = llm.invoke("Please answer this question: ", user_question)
-    return answer
