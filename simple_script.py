cae20e2535407d341935f77d71ae25f5ecec49cb
STAISHA NEVILLE <sjnevill@uwaterloo.ca>
2025-11-09 19:36:29 -0500

    Tested Pyodide UI with simple Ollama/Langchain function

--- simple_script.py
+++ simple_script.py
@@ -1,3 +1,6 @@
-def simple_script(user_input):
-    output = f"You entered: {user_input.upper()}"
-    return output
+def simple_script(user_question):
+    from langchain_ollama import OllamaLLM
+    llm = OllamaLLM(model="llama3.2")
+    
+    answer = llm.invoke("Please answer this question: ", user_question)
+    return answer
